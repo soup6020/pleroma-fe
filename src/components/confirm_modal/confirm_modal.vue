0684f19d1b903a7d941aa7cfa366044b98d97659
Tusooa Zhu <tusooa@kazv.moe>
2022-02-09 15:50:25 -0500

    Add ConfirmModal comp

---
+++ src/components/confirm_modal/confirm_modal.vue
@@ -0,0 +1,28 @@
+<template>
+  <dialog-modal
+    v-if="showing"
+    :onCancel="onCancel"
+  >
+    <template v-slot:header>
+      <span v-text="title"></span>
+    </template>
+
+    <slot></slot>
+
+    <template v-slot:footer>
+      <button
+        class="btn button-default"
+        v-text="confirmText"
+        @click.prevent="onAccept"
+      ></button>
+
+      <button
+        class="btn button-default"
+        v-text="cancelText"
+        @click.prevent="onCancel"
+      ></button>
+    </template>
+  </dialog-modal>
+</template>
+
+<script src="./confirm_modal.js"></script>
