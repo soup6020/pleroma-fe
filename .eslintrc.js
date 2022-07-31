d083931ca83fd9983022c63d6cabf4f5fc62b13d
Henry Jameson <me@hjkos.com>
2022-07-31 12:15:44 +0300

    update eslint stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     sourceType: 'module'
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
