ee066c4436702851cb8c2f144fed3293b532fa2c
Henry Jameson <me@hjkos.com>
2022-07-31 12:39:34 +0300

    fix filesizeformat const/let stuff

--- src/services/file_size_format/file_size_format.js
+++ src/services/file_size_format/file_size_format.js
@@ -1,14 +1,13 @@
-const fileSizeFormat = (num) => {
-  let exponent
-  let unit
+const fileSizeFormat = (numArg) => {
   const units = ['B', 'KiB', 'MiB', 'GiB', 'TiB']
+  let num = numArg
   if (num < 1) {
     return num + ' ' + units[0]
   }
 
-  exponent = Math.min(Math.floor(Math.log(num) / Math.log(1024)), units.length - 1)
+  const exponent = Math.min(Math.floor(Math.log(num) / Math.log(1024)), units.length - 1)
   num = (num / Math.pow(1024, exponent)).toFixed(2) * 1
-  unit = units[exponent]
+  const unit = units[exponent]
   return { num, unit }
 }
 const fileSizeFormatService = {
