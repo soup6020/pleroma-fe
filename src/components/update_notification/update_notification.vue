502d76ebcd87dc29f03ded587fe5c92d655931a7
Henry Jameson <me@hjkos.com>
2022-08-08 02:14:43 +0300

    lint

--- src/components/update_notification/update_notification.vue
+++ src/components/update_notification/update_notification.vue
@@ -14,45 +14,66 @@
         </span>
       </div>
       <div class="panel-body">
-        <div class="content" ref="content">
-          <img class="pleroma-tan" :src="pleromaTanVariant" :style="pleromaTanStyles"/>
-          <div class="spacer-top"/>
+        <div
+          ref="content"
+          class="content"
+        >
+          <img
+            class="pleroma-tan"
+            :src="pleromaTanVariant"
+            :style="pleromaTanStyles"
+          >
+          <div class="spacer-top" />
           <div class="text">
             <p>
-                {{ $t('update.big_update_content') }}
+              {{ $t('update.big_update_content') }}
             </p>
-            <i18n-t keypath="update.update_bugs" tag="p" class="extra-info">
+            <i18n-t
+              keypath="update.update_bugs"
+              tag="p"
+              class="extra-info"
+            >
               <template #pleromaGitlab>
-                <a target="_blank" href="https://git.pleroma.social/">{{ $t('update.update_bugs_gitlab') }}</a>
+                <a
+                  target="_blank"
+                  href="https://git.pleroma.social/"
+                >{{ $t('update.update_bugs_gitlab') }}</a>
               </template>
             </i18n-t>
-            <i18n-t keypath="update.update_changelog" tag="p" class="extra-info">
+            <i18n-t
+              keypath="update.update_changelog"
+              tag="p"
+              class="extra-info"
+            >
               <template #theFullChangelog>
-                <a target="_blank" href="https://pleroma.social/announcements/">{{ $t('update.update_changelog_here') }}</a>
+                <a
+                  target="_blank"
+                  href="https://pleroma.social/announcements/"
+                >{{ $t('update.update_changelog_here') }}</a>
               </template>
             </i18n-t>
           </div>
-          <div class="spacer-bottom"/>
+          <div class="spacer-bottom" />
         </div>
       </div>
       <div class="panel-footer">
         <button
           class="button-default"
           @click.prevent="neverShowAgain"
-          >
+        >
           {{ $t("general.never_show_again") }}
         </button>
         <button
+          v-if="!showingMore"
           class="button-default"
           @click.prevent="toggleShow"
-          v-if="!showingMore"
-          >
+        >
           {{ $t("general.show_more") }}
         </button>
         <button
           class="button-default"
           @click.prevent="dismiss"
-          >
+        >
           {{ $t("general.dismiss") }}
         </button>
       </div>
