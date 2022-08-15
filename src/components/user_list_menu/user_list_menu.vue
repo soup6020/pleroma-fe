8d6e5c1e69b32bc6c13558dc4346407662849486
Henry Jameson <me@hjkos.com>
2022-08-15 23:33:07 +0300

    whoops, forgot the most important bit

---
+++ src/components/user_list_menu/user_list_menu.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="UserListMenu">
+    <Popover
+      trigger="click"
+      placement="left"
+      remove-padding
+    >
+      <template #content>
+        <div class="dropdown-menu">
+            <button
+              v-for="list in lists"
+              :key="list.id"
+              class="button-default dropdown-item"
+              @click="toggleList(list.id)"
+            >
+              <span
+                class="menu-checkbox"
+                :class="{ 'menu-checkbox-checked': list.inList }"
+              />
+              {{ list.title }}
+            </button>
+        </div>
+      </template>
+      <template #trigger>
+        <button class="btn button-default dropdown-item">
+          {{ $t('lists.manage_lists') }}
+          <FAIcon
+            size="lg"
+            icon="chevron-right"
+          />
+        </button>
+      </template>
+    </Popover>
+  </div>
+</template>
+
+<script src="./user_list_menu.js"></script>
