8ecb67230c87754d43c4255c56c05566c8c53feb
Henry Jameson <me@hjkos.com>
2022-08-31 00:48:38 +0300

    make custom router-link for nav-entry to un-nest the interactive elements

---
+++ src/components/optional_router_link/optional_router_link.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link
+    v-if="to"
+    :to="to"
+    custom
+    v-slot="props"
+  >
+    <slot
+      v-bind="props"
+    />
+  </router-link>
+  <slot
+    v-else
+    v-bind="{}"
+  />
+</template>
+
+<script>
+export default {
+  props: ['to']
+}
+</script>
