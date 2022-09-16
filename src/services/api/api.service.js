12d8d1711bb41b14c35914cb82a6d5f41943e198
Sean King <seanking2919@protonmail.com>
2022-09-15 22:02:58 -0600

    Added support for removing users from followers

--- src/services/api/api.service.js
+++ src/services/api/api.service.js
@@ -67,6 +67,7 @@ const MASTODON_BLOCK_USER_URL = id => `/api/v1/accounts/${id}/block`
 const MASTODON_UNBLOCK_USER_URL = id => `/api/v1/accounts/${id}/unblock`
 const MASTODON_MUTE_USER_URL = id => `/api/v1/accounts/${id}/mute`
 const MASTODON_UNMUTE_USER_URL = id => `/api/v1/accounts/${id}/unmute`
+const MASTODON_REMOVE_USER_FROM_FOLLOWERS = id => `/api/v1/accounts/${id}/remove_from_followers`
 const MASTODON_SUBSCRIBE_USER = id => `/api/v1/pleroma/accounts/${id}/subscribe`
 const MASTODON_UNSUBSCRIBE_USER = id => `/api/v1/pleroma/accounts/${id}/unsubscribe`
 const MASTODON_BOOKMARK_STATUS_URL = id => `/api/v1/statuses/${id}/bookmark`
@@ -302,6 +303,13 @@ const blockUser = ({ id, credentials }) => {
 
 const unblockUser = ({ id, credentials }) => {
   return fetch(MASTODON_UNBLOCK_USER_URL(id), {
+    headers: authHeaders(credentials),
+    method: 'POST'
+  }).then((data) => data.json())
+}
+
+const removeUserFromFollowers = ({ id, credentials }) => {
+  return fetch(MASTODON_REMOVE_USER_FROM_FOLLOWERS(id), {
     headers: authHeaders(credentials),
     method: 'POST'
   }).then((data) => data.json())
@@ -1588,6 +1596,7 @@ const apiService = {
   unmuteConversation,
   blockUser,
   unblockUser,
+  removeUserFromFollowers,
   fetchUser,
   fetchUserByName,
   fetchUserRelationship,
