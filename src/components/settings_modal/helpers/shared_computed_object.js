947b73f8702ff4b7c5f74620204e86a4f375315c
Henry Jameson <me@hjkos.com>
2023-03-12 16:55:39 +0200

    minimize the rest of the sharedcomputedobject

--- src/components/settings_modal/helpers/shared_computed_object.js
+++ src/components/settings_modal/helpers/shared_computed_object.js
@@ -1,18 +1,10 @@
-import { defaultState as configDefaultState } from 'src/modules/config.js'
-
 const SharedComputedObject = () => ({
   user () {
     return this.$store.state.users.currentUser
   },
-  // Generating computed values for vuex properties
-  ...Object.keys(configDefaultState)
-    .map(key => [key, {
-      get () { return this.$store.getters.mergedConfig[key] },
-      set (value) {
-        this.$store.dispatch('setOption', { name: key, value })
-      }
-    }])
-    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {})
+  expertLevel () {
+    return this.$store.getters.mergedConfig.expertLevel > 0
+  }
 })
 
 export default SharedComputedObject
